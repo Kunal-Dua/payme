6c750054ebe19decda4e37eec82f8b12d1bceeb0
Kunal Dua <kunaldua2000@gmail.com>
2026-02-08 09:43:21 +0530

    init

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Appbar } from "@repo/ui/Appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { JSX } from "react";
+
+export default function Page(): JSX.Element {
+    const session = useSession();
+    return (
+        <div>
+            <Appbar
+                onSignin={signIn}
+                onSignout={signOut}
+                user={session.data?.user}
+            />
+        </div>
+    );
+}
